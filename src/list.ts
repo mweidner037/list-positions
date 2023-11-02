98b600eee084712be92eaad4db61eb388cef7702
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2023-11-01 20:35:42 -0400

    easy functions

--- src/list.ts
+++ src/list.ts
@@ -1,6 +1,8 @@
+import { IDs } from "./ids";
+
 export type Position = {
   readonly creatorID: string;
-  readonly timestamp: string;
+  readonly timestamp: number;
   readonly valueIndex: number;
 };
 
@@ -10,45 +12,169 @@ export type MetaEntry = {
   readonly parent: Position;
 };
 
+/**
+ * Info about a waypoint's values within a LocalList.
+ */
+interface WaypointInfo<T> {
+  readonly parent: Position;
+  /**
+   * The total number of present values at this
+   * waypoint and its descendants.
+   */
+  total: number;
+  /**
+   * The values (or not) at the waypoint's positions,
+   * in order from left to right, represented as
+   * an array of "items": T[] for present values,
+   * positive count for deleted values.
+   *
+   * The items always alternate types. If the last
+   * item would be a number (deleted), it is omitted.
+   *
+   * TODO: omit when empty?
+   */
+  items: (T[] | number)[];
+}
+
 export class List<T> {
   // Can't be set etc., but can be insertAfter'd or appear in a Cursor.
   // Or should we just use null for that?
-  readonly rootPos: Position;
+  readonly rootPos: Position = {
+    creatorID: "ROOT",
+    timestamp: 0,
+    valueIndex: 0,
+  };
+  private readonly rootInfo = {
+    parent: this.rootPos,
+    total: 0,
+    items: [],
+  };
+
+  readonly ID: string;
+  private timestamp = 0;
+
+  /**
+   * Maps from (creatorID, timestamp) to that waypoint's info.
+   */
+  private readonly state = new Map<string, Map<number, WaypointInfo<T>>>();
+
+  constructor(options?: { ID?: string }) {
+    if (options?.ID !== undefined) {
+      IDs.validate(options.ID);
+    }
+    this.ID = options?.ID ?? IDs.random();
+
+    this.state.set(
+      this.rootPos.creatorID,
+      new Map([[this.rootPos.timestamp, this.rootInfo]])
+    );
+  }
 
   // TODO: events: on, off. Has-a instead of subclass?
   // Meta mgmt & normal list changes.
 
-  addMetas(metas: Iterable<MetaEntry> | MetaEntry): void {
-    // TODO
+  addMeta(meta: MetaEntry): void {
+    let byCreator = this.state.get(meta.creatorID);
+    if (byCreator === undefined) {
+      byCreator = new Map();
+      this.state.set(meta.creatorID, byCreator);
+    }
+
+    const existing = byCreator.get(meta.timestamp);
+    if (existing === undefined) {
+      // New MetaEntry.
+      this.validate(meta.parent);
+      byCreator.set(meta.timestamp, {
+        parent: meta.parent,
+        total: 0,
+        items: [],
+      });
+    } else {
+      // Redundant MetaEntry. Make sure it matches existing.
+      if (!this.posEqual(meta.parent, existing.parent)) {
+        throw new Error(
+          `MetaEntry added twice with different parents: existing = ${JSON.stringify(
+            existing.parent
+          )}, new = ${JSON.stringify(meta.parent)}`
+        );
+      }
+    }
   }
 
-  getMetas(): MetaEntry[] {}
+  addMetas(metas: Iterable<MetaEntry>): void {
+    for (const meta of metas) this.addMeta(meta);
+  }
 
-  set(index: number, value: T): Position {}
+  *metas(): IterableIterator<MetaEntry> {
+    for (const [creatorID, byCreator] of this.state) {
+      for (const [timestamp, info] of byCreator) {
+        yield { creatorID, timestamp, parent: info.parent };
+      }
+    }
+  }
+
+  updateTimestamp(otherTimestamp: number): number {
+    this.timestamp = Math.max(otherTimestamp, this.timestamp);
+    return this.timestamp;
+  }
+
+  set(index: number, value: T): Position {
+    const pos = this.positionOfIndex(index);
+    this.setAt(pos, value);
+    return pos;
+  }
 
   setAt(pos: Position, value: T): void {}
 
   // For loading quickly
   setBulk(startPos: Position, values: T[]): void {}
 
-  insert(index: number, value: T): { pos: Position; meta: MetaEntry | null } {}
+  insert(index: number, value: T): { pos: Position; meta: MetaEntry | null } {
+    if (index < 0 || index > this.length) {
+      throw new Error(
+        `index out of bounds for insert: ${index}, length=${this.length}`
+      );
+    }
+
+    const prevPos =
+      index === 0 ? this.rootPos : this.positionOfIndex(index - 1);
+    return this.insertAfter(prevPos, value);
+  }
 
   insertAfter(
-    pos: Position,
+    prevPos: Position,
     value: T
   ): { pos: Position; meta: MetaEntry | null } {}
 
-  delete(index: number): Position {}
+  delete(index: number): Position {
+    const pos = this.positionOfIndex(index);
+    this.deleteAt(pos);
+    return pos;
+  }
 
   deleteAt(pos: Position): void {}
 
   clear(): void {}
 
-  get(index: number): T {}
+  get(index: number): T {
+    const pos = this.positionOfIndex(index);
+    return this.getAt(pos)!;
+  }
 
   getAt(pos: Position): T | undefined {}
 
-  get length(): number {}
+  hasAt(pos: Position): boolean {}
+
+  get length(): number {
+    return this.rootInfo.total;
+  }
+
+  positionOfIndex(index: number): Position {}
+
+  indexOfPosition(
+    pos: Position,
+    searchDir: "none" | "left" | "right" = "none"
+  ): number {}
 
   /**
    * Returns an iterator for values in the list, in list order.
