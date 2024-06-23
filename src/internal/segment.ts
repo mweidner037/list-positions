57cd9f400808febe29d45d0063a24e18b5bf48a9
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2024-06-23 08:58:04 -0400

    working on overwrite method

--- src/internal/segment.ts
+++ src/internal/segment.ts
@@ -1,19 +1,105 @@
 export abstract class Segment {
   next: Segment | null = null;
   abstract readonly isDeleted: boolean;
-  abstract readonly canAppend: boolean;
+  abstract readonly isMergeable: boolean;
+  // TODO: in tests, check always > 0.
   abstract readonly length: number;
   /**
+   * Set this one to the first half in-place; return a new second half. Don't update next pointers.
    *
    * @param index 0 < index < length
    */
-  abstract trimFront(index: number): Segment;
+  abstract splitContent(index: number): Segment;
   /**
-   *
-   * @param index 0 < index < length
+   * Merge other's content with ours (appending other).
    */
-  abstract trimBack(index: number): Segment;
-  abstract append(other: this): void;
+  abstract mergeContent(other: this): void;
+}
+
+/**
+ * Splits the segment at the given offset (if needed).
+ *
+ * @param offset 0 < offset <= segment.length
+ */
+function split(segment: Segment, offset: number) {
+  if (offset !== segment.length) {
+    const after = segment.splitContent(offset);
+    after.next = segment.next;
+    segment.next = after;
+  }
+}
+
+/**
+ * Connects after to before in the list, merging if possible.
+ * Returns the final segment, whose next pointer is *not* updated.
+ */
+function append(before: Segment, after: Segment): Segment {
+  // TODO: if this safe?
+  if (before.constructor === after.constructor && before.isMergeable) {
+    before.mergeContent(after);
+    return before;
+  } else {
+    before.next = after;
+    return after;
+  }
+}
+
+// // Includes index 0, not length.
+// function locate1(
+//   start: Segment,
+//   delta: number
+// ): [segment: Segment, offset: number, inside: boolean] {
+//   let current = start;
+//   let remaining = delta;
+//   for (; current.next !== null; current = current.next) {
+//     if (remaining < current.length) {
+//       return [current, remaining, true];
+//     }
+//     remaining -= current.length;
+//   }
+//   return [current, remaining, remaining < current.length];
+// }
+
+// Includes length, not index 0.
+// TODO: if delta is zero (e.g. insertion at 0), then offset will be 0 - improper.
+function locate(
+  start: Segment,
+  delta: number
+): [segment: Segment, offset: number, inside: boolean] {
+  let current = start;
+  let remaining = delta;
+  for (; current.next !== null; current = current.next) {
+    if (remaining <= current.length) {
+      return [current, remaining, true];
+    }
+    remaining -= current.length;
+  }
+  return [current, remaining, remaining <= current.length];
+}
+
+export class SegmentList {
+  head: Segment | null = null;
+
+  // TODO: trim deleted ends
+  // TODO: check for no-deleted-ends in tests
+  // TODO: if segment is externally accessible or settable, need to defend against aliasing+mutation.
+  // Likewise for whole list.
+
+  overwrite(index: number, segment: Segment): void {
+    if (this.head === null) {
+      this.head = new DeletedSegment(index + segment.length);
+    }
+
+    // eslint-disable-next-line prefer-const
+    let [left, leftOffset, leftInside] = locate(this.head, index);
+    if (!leftInside) {
+      const preLeft = left;
+      left = new DeletedSegment(leftOffset + segment.length - preLeft.length);
+      append(preLeft, left);
+      leftOffset -= segment.length;
+    }
+    split(left, leftOffset);
+  }
 }
 
 export class DeletedSegment extends Segment {
@@ -25,7 +111,7 @@ export class DeletedSegment extends Segment {
     return true;
   }
 
-  get canAppend() {
+  get isMergeable() {
     return true;
   }
 
@@ -37,7 +123,7 @@ export class DeletedSegment extends Segment {
     return new DeletedSegment(index);
   }
 
-  append(other: this): void {
+  mergeContent(other: this): void {
     this.length += other.length;
   }
 }
@@ -51,7 +137,7 @@ export class ArraySegment<T> extends Segment {
     return false;
   }
 
-  get canAppend() {
+  get isMergeable() {
     return true;
   }
 
@@ -67,7 +153,7 @@ export class ArraySegment<T> extends Segment {
     return new ArraySegment(this.values.slice(0, index));
   }
 
-  append(other: this): void {
+  mergeContent(other: this): void {
     this.values.push(...other.values);
   }
 }
@@ -81,7 +167,7 @@ export class StringSegment extends Segment {
     return false;
   }
 
-  get canAppend() {
+  get isMergeable() {
     return true;
   }
 
@@ -97,7 +183,7 @@ export class StringSegment extends Segment {
     return new StringSegment(this.chars.slice(0, index));
   }
 
-  append(other: this): void {
+  mergeContent(other: this): void {
     this.chars = this.chars + other.chars;
   }
 }
@@ -111,7 +197,7 @@ export class PresentSegment extends Segment {
     return false;
   }
 
-  get canAppend() {
+  get isMergeable() {
     return true;
   }
 
@@ -123,7 +209,7 @@ export class PresentSegment extends Segment {
     return new PresentSegment(index);
   }
 
-  append(other: this): void {
+  mergeContent(other: this): void {
     this.length += other.length;
   }
 }
@@ -137,7 +223,7 @@ export class AtomSegment<A> extends Segment {
     return false;
   }
 
-  get canAppend() {
+  get isMergeable() {
     return false;
   }
 
@@ -153,7 +239,7 @@ export class AtomSegment<A> extends Segment {
     throw new Error("Invalid for AtomSegment");
   }
 
-  append(): void {
+  mergeContent(): void {
     throw new Error("Invalid for AtomSegment");
   }
 }
